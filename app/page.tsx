66c910e6424dc2e370cf274a15d4b06b8785aacb
rileyhalcomb <rileyhalcomb@gmail.com>
2024-02-24 23:32:33 -0600

    Update minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
                     &nbsp;
                     <code className="font-mono font-bold" style={{fontFamily: "minecraftmedium"}}>NODE.JS is not fun.</code>
                 </p>
+
                 <div
                     className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
                     <Link href="https://www.rowdyhacks.org" passHref
