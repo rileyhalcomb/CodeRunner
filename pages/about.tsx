1c67081d6c6dfd79441087ddab31edb4ba99974a
rileyhalcomb <rileyhalcomb@gmail.com>
2024-02-24 23:50:16 -0600

    Update minor changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import Head from "next/head";
 import Image from "next/image";
 
 import '../app/globals.css';
 import Link from "next/link";
 
-
 const AboutPage: React.FC = () => {
     return (
         <>
+            <Head>
+                <meta name="About the Creators" content="Created by Riley Halcomb and Dylan Davis"/>
+                <title>About the Creators</title>
+            </Head>
+
+
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <div
                     className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] animate-pulse">
@@ -44,6 +50,7 @@ const AboutPage: React.FC = () => {
 
                 <div className="mb-32 grid text-center lg:max-w-4xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
                     <Link href="https://www.linkedin.com/in/rileyhalcomb/" passHref
+                          target="_blank" rel="noopener noreferrer"
                           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
                         <h2 className="mb-3 text-2xl font-semibold" style={{fontFamily: "minecraftmedium"}}>
                             RILEY HALCOMB - LINKEDIN{' '}
@@ -58,6 +65,7 @@ const AboutPage: React.FC = () => {
                     </Link>
 
                     <Link href="https://www.linkedin.com/in/dylan-davis--/" passHref
+                          target="_blank" rel="noopener noreferrer"
                           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
                         <h2 className="mb-3 text-2xl font-semibold" style={{fontFamily: "minecraftmedium"}}>
                             DYLAN DAVIS - LINKEDIN{' '}
--- pages/database.tsx
+++ pages/database.tsx
@@ -3,10 +3,16 @@ import Link from "next/link";
 
 import "../app/globals.css"
 import Image from "next/image";
+import Head from "next/head";
 
 const DatabasesPage: React.FC = () => {
     return (
         <>
+            <Head>
+                <meta name="CR Database" content="Created by Riley Halcomb and Dylan Davis"/>
+                <title>CR Database</title>
+            </Head>
+
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <div
                     className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] animate-pulse">
